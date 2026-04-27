c67cef407ca5d490a626ecc7ca624d93436ed2f8
Test Name <test@mail.ru>
2026-04-27 19:36:01 +0300

    Created contact:hero and added contact:partners

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -2,6 +2,8 @@ import '../assets/scss/pricing.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
 
 useTheme();
 useBurger();
+usePriceSwitcher();
