9c8ab556a4e927688ef8e5558019f10d763341aa
Test Name <test@mail.ru>
2026-05-06 13:24:15 +0300

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -10,18 +10,18 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  // publicDir: './src/assets',
+  publicDir: './src/assets',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './src/index.html',
         about: './src/about.html',
-        blog: './src/blog.html',
-        posts: './src/posts.html',
         pricing: './src/pricing.html',
         contact: './src/contact.html',
+        blog: './src/blog.html',
+        posts: './src/posts.html',
       },
     },
   },
@@ -30,11 +30,9 @@ export default defineConfig({
     ViteMinifyPlugin(),
     ViteImageOptimizer({
       png: {
-        // https://sharp.pixelplumbing.com/api-output#png
         quality: 80,
       },
       jpg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
         quality: 80,
       },
     }),
